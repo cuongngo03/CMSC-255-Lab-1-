9a4d10c1c0c83a5bb3ec446ac343fa4e08396910
cuongngo03 <46724338+cuongngo03@users.noreply.github.com>
2019-01-15 13:54:48 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,27 @@
+/***********************************************
+ * Java Program Name: Hello.java
+ ***********************************************
+ * Project/Program description: First lab
+ * ______________________________________________
+ * Tuan Cuong Ngo
+ * 01/15/19
+ * CMSC-255-002
+ **********************************************/
+
+public class Hello {
+ public static void printHeading() {
+  System.out.println("Tuan Cuong Ngo");
+  System.out.println("First lab");
+  System.out.println("Project description: Intro to programming Lab 1");
+  System.out.println("Version Date: 01/15/19");
+  System.out.println("CMSC-255-002");
+  System.out.println();
+ }
+
+public static void main(String [] args) {
+  printHeading();
+  System.out.println("Hello");
+ }
+}
+
+  
